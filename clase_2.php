e8585c443ddabc9e25c58526453c4352c666eb87
César Cancino <yo@cesarcancino.com>
2018-04-07 12:37:08 -0300

    código

--- clase_2.php
+++ clase_2.php
@@ -1,9 +1,41 @@
 <?php
 if(isset($_POST["nombre"]) )
 {
-	echo $_POST["nombre"];
-	echo "<br />";
-	echo $_POST["correo"];
+	
+	 
+	if(filter_var( trim( $_POST["nombre"] )  ) == false)
+	{
+		echo "el campo nombre viene vacío";
+		exit;
+	}
+
+
+
+	$pais = "";
+	$genero ="";
+	switch($_POST["pais"])
+	{
+		case '1':
+			$pais="Chile";
+		break;
+		case '2':
+			$pais="Venezuela";
+		break;
+		case '3':
+			$pais="Costarica";
+		break;
+		case '4':
+			$pais="Perú";
+		break;
+	} 
+	if($_POST["genero"]=='m')
+	{
+		$genero="Masculino";
+	}else
+	{	
+		$genero="Femenino";
+	}
+	echo "Su país es ".$pais." y su género es ".$genero;
 	exit;//aquí se detiene la ejecución del script
 }
 ?>
@@ -24,6 +56,22 @@ if(isset($_POST["nombre"]) )
 		</p>
 		<p>
 			E-Mail: <input type="text" name="correo" />
+		</p>
+		
+		<p>
+			País : 
+			<select name="pais">
+				<option value="0">Seleccione.....</option>	
+				<option value="1">Chile</option>
+				<option value="2" selected="true">Venezuela</option>
+				<option value="3">Costa Rica</option>
+				<option value="4">Perú</option>
+			</select>
+		</p>
+		<p>
+			Género : 
+			Masculino: <input type="radio" name="genero" value="m" checked="true" />
+			Femenino: <input type="radio" name="genero" value="f" />
 		</p>
 		<p>
 			<input type="submit" value="Enviar" />
