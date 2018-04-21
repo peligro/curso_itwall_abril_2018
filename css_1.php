17c87b92790ee90102808534ae5948f31819f562
César Cancino <yo@cesarcancino.com>
2018-04-21 16:59:14 -0300

    csss1

---
+++ css_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Ejemplo CSS</title>
+	<link rel="stylesheet" type="text/css" href="public/css/estilos.css" />
+</head>
+<body>
+
+	<p>texto dentro del párrafo ñandú</p>
+	<hr />
+	<p>texto dentro del párrafo ñandú</p>
+	<hr />
+	<p>texto dentro del párrafo ñandú</p>
+	<hr />
+	<div>texto del div</div>
+
+</body>
+</html>
