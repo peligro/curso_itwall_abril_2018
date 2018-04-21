cb7d3317decdd86d6d1d0959dab1563fc5e3603c
César Cancino <yo@cesarcancino.com>
2018-04-21 11:27:38 -0300

    kk

--- comandos_bd.php
+++ comandos_bd.php
@@ -1,1 +1,11 @@
 create database itwall character set utf8 collate utf8_spanish_ci;
+
+create table
+(
+	id int auto_increment not null primary key,
+	nombre varchar(100),
+	rut varchar(20),
+	correo varchar(100),
+	fecha date,
+	estado int
+);
