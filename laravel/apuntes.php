877cb490e8b7b3b098b77719b9100ffc1831c1d4
César Cancino <yo@cesarcancino.com>
2018-04-28 16:29:04 -0300

    proyecto laravel

---
+++ laravel/apuntes.php
@@ -0,0 +1,46 @@
+  
+  - framework
+
+  	* frame work 	
+
+  		+ patrones: son convenciones mas formales 
+
+  			* MVC
+  			* MVT
+  			* ORM
+  			* Active Record
+  			* Factory
+  			* Builder Constructor
+  			* Singleton
+  			* Validator
+  			* Routes
+  			* Security
+  			* Auth -> Middleware ->Token
+  			* Templates
+
+  		+ convenciones: 
+  		+ helpers : son grupos de funciones útiles
+  		+ clases : 
+  		+ Librerías : Auth, Validaciones, Rutas, Helpers, Controllers, Modelos, Migraciones. Además,  los framewoks permiten implementar librerías de terceros.
+  		+ URLs : Debo informale y configurarle todas las peticiones HTTP al framework, entendiendo que tengo disponibles distintos métodos HTTP.
+
+  		http://www.dominio.com/index.php?controller=algo%vista=algo&parametro=1
+
+  		http://www.dominio.com/algo/algo/1
+
+  - composer rpm : la consola de laravel se llama : artisan
+
+  - migraciones : migrate - seeders
+
+  - MVC (MVT = Modelo Vista Templates)
+
+
+  		- Controller : Puente entre el modelo y la vista. Desde el controlador se generan las funciones que permiten desplegar las URLs generadas en las rutas
+  		- Models : Son las clases que representan las estructura de las tablas de mi Base de datos.
+  		- Views :Representan todo el backend y frontend visual para el usuario final.
+  		- Templates
+
+
+  - ORM
+
+  - 
